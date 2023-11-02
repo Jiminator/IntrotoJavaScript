ff23b915d937834d8d17abb8f77b549517187188
Jimmy Shong <jimmysh341@gmail.com>
2023-11-02 11:31:28 -0400

    Finished Assignment

---
+++ script3.js
@@ -0,0 +1,5 @@
+function concatenateStrings(str1, str2) {
+    alert(str1 + str2);
+}
+
+concatenateStrings("Hello ", "World");
---
+++ script4.js
@@ -0,0 +1,11 @@
+function NumberPolarity(number) {
+    if (number === 0) {
+        alert("The value is zero.");
+    } else if (number > 0) {
+        alert("The value is positive.");
+    } else {
+        alert("The value is negative.");
+    }
+}
+
+NumberPolarity(0); 
